0ef4f703ae9d658f6f3d602ece4d4c43bffdbba0
Aljon Preza <aljonpreza@yahoo.com>
2015-11-16 09:57:03 -1000

    Finished EditFlight

--- app/client/templates/stuff/ListFlight.js
+++ app/client/templates/stuff/ListFlight.js
@@ -6,7 +6,7 @@ Template.ListFlight.helpers({
   /**
    * @returns {*} All of the Stuff documents.
    */
-  stuffList: function () {
+  flightList: function () {
     return Flight.find();
   }
 });
